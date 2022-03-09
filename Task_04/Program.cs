9ca7a511eecc6b7c4c581b171fac73a00afc523d
darlyncarmenate <darlyncarmenate@gmail.com>
2022-03-09 12:02:19 +0200

    Task 04

---
+++ Task_04/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите цифу от 1 до 7: ");
+string first = Console.ReadLine();
+int a = int.Parse(first);
+
+if(a == 6 | a == 7)
+{
+    Console.WriteLine("Это выходной день");
+}
+if(a < 6)
+{
+    Console.WriteLine("Это рабочий день");
+}
