02c8b340e795dfa8579f1c8e484438c7b68d3170
darlyncarmenate <darlyncarmenate@gmail.com>
2022-03-09 11:36:59 +0200

    Task_03

---
+++ Task_03/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+string first = Console.ReadLine();
+int a = int.Parse(first);
+
+if(a < 100)
+{
+    Console.WriteLine("Третьей цифры не существует");
+}
+else
+{
+    while(a > 999)
+    {
+        a = a / 10;
+    }
+    int result = a % 10;
+    Console.WriteLine($"Третья цифра: {result}");
+}
