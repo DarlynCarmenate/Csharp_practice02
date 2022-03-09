37a91a41b3bd25147a5af6ef51bc29f29c824149
darlyncarmenate <darlyncarmenate@gmail.com>
2022-03-09 11:20:58 +0200

    Small changes

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -1,4 +1,4 @@
 ﻿int a = new Random().Next(100, 1000);
 int result = (a / 10) % 10;
-Console.WriteLine("Получает на ввод " + a);
+Console.WriteLine("Принимает на вход " + a);
 Console.WriteLine("Отдает " + result);
